a33438a5c42a886926bdb8fa9ebb574a280f7fbd
cka09191 <cka09191@gmail.com>
2025-08-10 12:36:35 +0900

    Fix script source path, update PostCSS and Autoprefixer versions, and clean up CSS files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,8 +5,8 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <button onClick={() => setCount((count) => count + 1)}>
+    <div>
+      <button onClick={() => setCount((count) => count + 1)} className="m-10 px-4 py-2 border-2 border-gray-400 rounded hover:bg-gray-100">
         count is {count}
       </button>
     </div>
